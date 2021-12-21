d37a11ff7ed512daedb7d313a62583fa3e48ec4e
Abhik Das <abhikdas98765@gmail.com>
2021-12-21 16:56:57 +0530

    redesigned navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light container">
+      <a className="navbar-brand font-weight-bold" href="#">Foodies</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Dine In
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Drinks
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+
+        <button className="btn register-btn" type="submit">
+          Register Now
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
